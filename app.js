318b849e07fbac64e0e62bba5983cc98280307b9
KimAnatolii <WitcherUSS@gmail.com>
2020-03-04 12:20:25 +0300

    write to excel

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ require('chromedriver');
 const chrome = require('selenium-webdriver/chrome');
 const {Builder, By, until} = require('selenium-webdriver');
 const webdriver = require('selenium-webdriver');
+const Excel = require('exceljs');
 const readXlsxFile = require('read-excel-file/node');
 
 var driver = new Builder()
   .forBrowser('chrome')
   .setChromeOptions(new chrome.Options()
-    // .addArguments('--headless')
     .addArguments('--no-sandbox')
     .addArguments('--disable-dev-shm-usage')
     .addArguments("--lang=ru")
@@ -19,23 +19,42 @@ readXlsxFile('./users.xlsx').then((rows) => {
   getName(rows);
 });
 
-
 //функция заполнения полей.
 function getName(rows) {
+
+  async function exTest(){
+    const workbook = new Excel.Workbook();
+    const worksheet = workbook.addWorksheet("My Sheet");
+    worksheet.columns = [
+      {header: 'Name', key: 'name1', width: 35},
+      {header: 'Email', key: 'email1', width: 35}, 
+      {header: 'Pass', key: 'password1', width: 35},
+      {header: 'Reg Time', key: 'time', width: 20}
+    ];
   let column = rows.shift();
+
+let name = column[0];
+let email = column[1];
+let password = column[2];
+var now = new Date();
+
+worksheet.addRow({name1: name, email1: email, password1: password, time: now,});
+// save under export.xlsx
+workbook.xlsx.writeFile('Results.xlsx');
+
   driver.get('https://portal.kgainfo.spb.ru/KGAMap/Auth/Register').then(() => {
     driver.wait(
       until.elementLocated(By.xpath('//*[@id="fio"]')), 3000
-    ).sendKeys(column[0]).then(() => {
+    ).sendKeys(name).then(() => {
       driver.wait(
         until.elementLocated(By.xpath('//*[@id="username"]')), 3000
-      ).sendKeys(column[1]).then(() => {
+      ).sendKeys(email).then(() => {
         driver.wait(
           until.elementLocated(By.xpath('//*[@id="password1"]')), 3000
-        ).sendKeys(column[2]).then(() => {
+        ).sendKeys(password).then(() => {
           driver.wait(
             until.elementLocated(By.xpath('//*[@id="password2"]')), 3000
-          ).sendKeys(column[2]).then(() => {
+          ).sendKeys(password).then(() => {
             driver.wait(
               until.elementLocated(By.xpath('//*[@id="regform"]/div[5]/div[1]/span/button')), 3000
             ).click().then(() => {
@@ -43,13 +62,6 @@ function getName(rows) {
                 until.elementLocated(By.xpath('//*[@id="regButton"]')), 3000
               ).sendKeys(webdriver.Key.ENTER).then(() => {
                 getName(rows);
-                /*driver.wait(
-                    until.elementLocated(By.xpath('//*[@id="wob_d"]/div/div[2]/div[3]')), 3000
-                ).getAttribute('innerText').then((t5) => {
-                    console.log((str.charAt(0).toUpperCase() + str.slice(1)) + ': ' + t1 + '. Температура: ' + t2 + '(С). ' + t3 + '. ' + t4 + '. ' + t5);
-                    driver.quit();
-                }).catch(() => {
-                });*/
               }).catch(() => {
               });
             }).catch(() => {
@@ -62,4 +74,6 @@ function getName(rows) {
       });
     });
   });
+};
+exTest();
 }
